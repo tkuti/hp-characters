73a67d3a0b6796b7e562cab1239e6d615ffe80f2
tkuti <tunde.kuti@gmail.com>
2021-03-29 15:50:42 +0200

    character details

--- frontend/src/components/Character.js
+++ frontend/src/components/Character.js
@@ -1,20 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import CharacterDetails from './CharacterDetails'
 
- function Character({character}) {
+function Character({ character }) {
+
+    const [showDetails, setShowDetails] = useState(false)
+
+
 
     return (
-        <div className="card">
-            <h2>{character.name}</h2>
-            <div className="img">
-            <img src={character.image} alt="character"/>
+        <>
+            <div className="card">
+                <h2>{character.name}</h2>
+                <div className="img">
+                    <img src={character.image} alt="character" />
 
+                </div>
+                <p>role: {character.hogwartsStudent ? "student"
+                    : character.hogwartsStaff ? "staff" : "other"}</p>
+                <p>house : {character.house ? character.house : "-"}</p>
+                <p>actor: {character.actor}</p>
+                <button onClick={() => setShowDetails(true)}>More info..</button>
             </div>
-            <p>role: {character.hogwartsStudent ? "student" 
-            : character.hogwartsStaff ? "staff" : "other"}</p>
-            <p>house : {character.house ? character.house : "-"}</p>
-            <p>actor: {character.actor}</p>
-            <button>More info..</button>
-        </div>
+            {showDetails && <CharacterDetails character={character} setShowDetails={setShowDetails} />}
+        </>
     )
 }
 
---
+++ frontend/src/components/CharacterDetails.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+function CharacterDetails({ character, setShowDetails }) {
+    return (
+        <div className="details-background" onClick={() => setShowDetails(false)}>
+            <div className="details-container">
+                <div className="details-img">
+                    <p className="name">{character.name}</p>
+                    <img src={character.image} alt="character"/>
+                </div>
+                <div className="details">
+                    <p className="detail-text">species: {character.species}</p>
+                    <p className="detail-text">gender: {character.gender}</p>
+                    <p className="detail-text">role: {character.hogwartsStudent ? "student"
+                        : character.hogwartsStaff ? "staff" : "other"}</p>
+                    {character.house && <p className="detail-text">house: {character.house}</p>}
+                    {character.yearOfBirth && <p className="detail-text">birth year: {character.yearOfBirth}</p>}
+                    {character.ancestry && <p className="detail-text">ancestry: {character.ancestry}</p>}
+                    {character.wand && <p className="detail-text">wand:
+                    <ul>
+                            <li>wood: {character.wand.wood}</li>
+                            <li>core: {character.wand.core}</li>
+                            <li>length: {character.wand.length}</li>
+                        </ul>
+                    </p>}
+                    {character.patronus && <p className="detail-text">patronus: {character.patronus}</p>}
+                    <p className="detail-text">actor: {character.actor}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CharacterDetails
