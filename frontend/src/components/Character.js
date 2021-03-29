a627570bd2e8f79db386dc85ad2304709daac51e
tkuti <tunde.kuti@gmail.com>
2021-03-29 13:45:13 +0200

    css layout

--- frontend/src/components/Character.js
+++ frontend/src/components/Character.js
@@ -5,11 +5,15 @@ import React from 'react'
     return (
         <div className="card">
             <h2>{character.name}</h2>
+            <div className="img">
             <img src={character.image} alt="character"/>
+
+            </div>
             <p>role: {character.hogwartsStudent ? "student" 
             : character.hogwartsStaff ? "staff" : "other"}</p>
             <p>house : {character.house ? character.house : "-"}</p>
             <p>actor: {character.actor}</p>
+            <button>More info..</button>
         </div>
     )
 }
